905edfb16c330864dd42a21def063a2944c7e4b1
Abdur-Rahman <ahmadabdulrahma103@gmail.com>
2024-10-01 15:30:00 +0100

    update

--- src/components/boxComponent.vue
+++ src/components/boxComponent.vue
@@ -2,12 +2,12 @@
     <div 
     :id="index"
     @click="play($event)"
-    :class="{'hover:bg-neutral-800' : !winnerCombination.length && playerOneAlphabet, 
-    'bg-green-300 text-black' : winnerCombination.length && winnerCombination.includes(index)}"
-    class="border border-zinc-700 shadow-sm shadow-black hover:shadow-none transition-all duration-[10ms] hover:text-[75px] text-gray-700 grid place-content-center text-[80px] bg-neutral-900 rounded-md caveat"></div>
+    :class="[!winnerCombination.length ? 'hover:bg-neutral-800' : '', winIndex ? 'bg-green-500' : '']"
+    class="border border-zinc-700 shadow-md shadow-black hover:shadow-sm transition-all duration-[.5s] hover:text-[75px] text-gray-700 grid place-content-center text-[80px] bg-neutral-900 rounded-md caveat"></div>
 </template>
 
 <script setup>
+import { watch } from 'vue';
 import { useGameStore } from '../stores/GameStore';
 import { storeToRefs } from 'pinia';
 
@@ -16,6 +16,7 @@ const {winnerCombination, playerOneAlphabet} = storeToRefs(gameStore)
 const {play} = gameStore
 
 const props = defineProps({
-    index: Number
+    index: Number,
+    winIndex: Boolean,
 })
 </script>
--- src/components/boxesComponent.vue
+++ src/components/boxesComponent.vue
@@ -2,8 +2,10 @@
         <div  class="relative z-30 pt-4 md:w-full md:h-full md:grid md:place-content-center">
             <div ref="boxesContainer" :class="{'gap-3': gameStore.isDarkMode}"  class="grid grid-cols-3 grid-rows-3 gap-1 md:w-[400px] md:h-[400px] w-[300px] min-w-[150px] h-[300px] justify-center m-auto ">
                 <boxComponent
-                v-for="index in 9" 
-                :key="index" :index="index" />
+                v-for="index in 9"
+                :key="index"
+                :windIndex="winnerCombination.length && winnerCombination.includes(index)"
+                :index="index" />
             </div>
             <div  v-if="playHasStarted || playerOneWins || playerTwoWins || drawTies" class="py-1 text-center text-sm">
                 <button class="p-1 px-6 m-3 text-white bg-green-800 hover:bg-green-600 rounded-full exo" @click="resetAndRestart()" type="">NEXT</button>
@@ -21,7 +23,7 @@ import { onMounted, ref, watch } from 'vue';
 const boxesContainer = ref([])
 
 const gameStore = useGameStore()
-const {playHasStarted, playerOneWins, boxes, playerTwoWins, drawTies, computerChoiceIndex, computerAlphabet, vsPC} = storeToRefs(gameStore)
+const {playHasStarted, playerOneWins, boxes, winnerCombination, playerTwoWins, drawTies, computerChoiceIndex, computerAlphabet, vsPC} = storeToRefs(gameStore)
 const {resetAndRestart, clearAndRestart} = gameStore
 
 onMounted(() => {
