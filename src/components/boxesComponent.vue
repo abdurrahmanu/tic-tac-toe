7023e4d17d487afec2c9b6305c46570bc73f5622
Ahmed Abdulrahman <104634941+abdurrahmanu@users.noreply.github.com>
2024-10-08 19:47:25 +0100

    Update boxesComponent.vue

--- src/components/boxesComponent.vue
+++ src/components/boxesComponent.vue
@@ -9,7 +9,7 @@
                 :index="index" />
             </div>
             <div  v-if="start || playerOneWins || playerTwoWins || drawTies" class="text-center text-sm space-x-1 py-4 flex">
-                <button class="p-1 px-12 text-white bg-yellow-800 hover:bg-yellow-600 rounded-l-full exo" @click="undoMove()" type="">UNDO</button>
+                <button class="p-1 px-8 text-white bg-yellow-800 hover:bg-yellow-600 rounded-l-full exo" @click="undoMove()" type="">UNDO</button>
                 <div class="relative">
                     <div class="text-center absolute bottom-[50px] left-[50%] translate-x-[-50%] rounded-md bg-red-400 p-2 shadow-sm shadow-black exo text-sm space-y-2 after:bg-red-700 after:z-[99999999] after:h-4 after-w-10 after:absolute after:top-[100%] after:left-[50%] after:translate-x-[-50%] after:border-r-0 after:border-l-0 after:border-t-1" v-if="openModal">
                         <p>RESET THIS GAME</p>
@@ -18,9 +18,9 @@
                             <p @click="restart(false)" class="px-10 py-2 border-l-0 border-[1px] hover:bg-red-500 border-red-900">NO</p>
                         </div>
                     </div>                    
-                    <button class="p-1 px-12 text-white bg-red-800 hover:bg-red-500 exo" @click="openModal = true" type="">RESET</button>
+                    <button class="p-1 px-8 text-white bg-red-800 hover:bg-red-500 exo" @click="openModal = true" type="">RESET</button>
                 </div>
-                <button class="p-1 px-12 text-white bg-green-900 hover:bg-green-600 rounded-r-full exo" @click="reset()" type="">NEXT</button>
+                <button class="p-1 px-8 text-white bg-green-900 hover:bg-green-600 rounded-r-full exo" @click="reset()" type="">NEXT</button>
             </div>
         </div>
 </template>
